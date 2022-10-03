3963e71a8bd95d7048690edf90e7c21aa41439e7
yaonggod <jy23698@naver.com>
2022-10-03 23:28:27 +0900

    1003

--- 4/벽부수고이동하기.py
+++ 4/벽부수고이동하기.py
@@ -3,42 +3,40 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-matrix = [list(input()) for _ in range(n)]
-visited = [[[-1, 0] for _ in range(m)] for _ in range(n)]
+matrix = [list(map(int, list(input().strip()))) for _ in range(n)]
+# visited[x][y][0]은 벽 파괴 가능한 루트, visited[x][y][1]은 불가능한 루트
+visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
 visited[0][0] = [1, 0]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-queue = deque([[0, 0]])
-while queue:
-    k = queue.popleft()
-    x = k[0]
-    y = k[1]
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        # 영역 내에 있고 안 간 곳이다
-        if 0 <= nx < n and 0 <= ny < m and visited[nx][ny][0] == -1:
-            # 벽을 부순 전적이 없다
-            if visited[x][y][1] == 0:
-                # 앞에 벽이 없다
-                if matrix[nx][ny] == '0':
-                    visited[nx][ny] = [visited[x][y][0] + 1, 0]
-                    queue.append([nx, ny])
-                # 앞에 벽이 있다 -> 부순다
-                else:
-                    visited[nx][ny] = [visited[x][y][0] + 1, 1]
-                    queue.append([nx, ny])
-            # 벽을 부순 전적이 있다
-            elif visited[x][y][1] == 1:
-                # 앞에 벽이 없다
-                if matrix[nx][ny] == '0':
-                    visited[nx][ny] = [visited[x][y][0] + 1, 1]
-                    queue.append([nx, ny])
-                # 앞에 벽이 있으면 못감
+def bfs(x, y, z):
+    queue = deque([[x, y, z]])
+    while queue:
+        k = queue.popleft()
+        x = k[0]
+        y = k[1]
+        z = k[2]
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][z]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < m:
+                # 벽 부순 적 없는데 벽이 나타났다
+                if matrix[nx][ny] == 1 and z == 0:
+                    visited[nx][ny][1] = visited[x][y][0] + 1
+                    queue.append([nx, ny, 1])
+                # 갈 수 있음
+                elif matrix[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                    visited[nx][ny][z] = visited[x][y][z] + 1
+                    queue.append([nx, ny, z])
+    return -1
+                
+print(bfs(0, 0, 0))
 
-print(visited[n - 1][m - 1][0])
+
 
 
 
