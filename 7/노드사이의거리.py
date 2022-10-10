f116cd4ffb2641f01869119a0f8a804217d6b179
yaonggod <jy23698@naver.com>
2022-10-10 23:53:29 +0900

    1010

--- 7/노드사이의거리.py
+++ 7/노드사이의거리.py
@@ -1,20 +1,28 @@
-# dfs
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n, m = map(int, input().split())
-INF = 1000000000
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-for _ in range(n + 1):
-    graph[_][_] = 0
+graph = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
-    i, j, length = map(int, input().split())
-    graph[i][j] = length
-    graph[j][i] = length
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    a, b, length = map(int, input().split())
+    graph[a].append([b, length])
+    graph[b].append([a, length])
 for _ in range(m):
     a, b = map(int, input().split())
-    print(graph[a][b])
+    visited = [False] * (n + 1)
+    visited[a] = True
+    queue = deque([[a, 0]])
+    while queue:
+        k = queue.popleft()
+        x = k[0]
+        length = k[1]
+        if x == b:
+            print(length)
+        for y in graph[x]:
+            if not visited[y[0]]:
+                visited[y[0]] = True
+                queue.append([y[0], length + y[1]])
+                
+            
+    
--- 7/알파벳.py
+++ 7/알파벳.py
@@ -1,24 +1,31 @@
 # 백트래킹
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 r, c = map(int, input().split())
 board = [list(input()) for _ in range(r)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-queue = deque([[0, 0, [board[0][0]]]])
-maxlen = 1
-while queue:
-    k = queue.popleft()
-    x = k[0]
-    y = k[1]
-    if len(k[2]) > maxlen:
-        maxlen = len(k[2])
+
+max_len = 1
+visited = [False] * 26
+visited[ord(board[0][0]) - 65] = True
+def backtracking(x, y, lst : list):
+    global max_len
+    global visited
+    if len(lst) > max_len:
+        max_len = len(lst)
+    
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
-        if 0 <= nx < r and 0 <= ny < c and board[nx][ny] not in k[2]:
-            lst = k[2] + [board[nx][ny]]
-            queue.append([nx, ny, lst])
-print(maxlen)
+        if 0 <= nx < r and 0 <= ny < c and not visited[ord(board[nx][ny]) - 65]:
+            visited[ord(board[nx][ny]) - 65] = True
+            lst.append(board[nx][ny])
+            backtracking(nx, ny, lst)
+            lst.pop()
+            visited[ord(board[nx][ny]) - 65] = False
+
+backtracking(0, 0, [board[0][0]])
+print(max_len)
+    
